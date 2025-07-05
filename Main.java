7ea3468b68da90d12b81a8cbcc82537d2a16c712
habibaaa682 <habibagalall2002@gmail.com>
2025-07-05 13:52:39 +0300

    apply another case

--- Main.java
+++ Main.java
@@ -4,11 +4,13 @@ public class Main {
         Product cheese = new Product("Cheese", 100, 5, true, LocalDate.of(2025, 7, 30), true, 0.2);
         Product biscuits = new Product("Biscuits", 150, 3, true, LocalDate.of(2025, 8, 15), true, 0.7);
         Product scratchCard = new Product("Scratch Card", 50, 10, false, null, false, 0);
+        Product tv = new Product("TV", 5000, 2, false, null, true, 10f);
         Customer customer = new Customer("sara", 6000);
         Cart cart = new Cart();
         cart.add(cheese, 2);
         cart.add(biscuits, 1);
         cart.add(scratchCard, 1);
+        cart.add(tv, 1);
         cart.checkout(customer);
     }
 }
